b20f0e12eb7b3e0e0da9e2fc1ea18870f4220f1e
Barret J. Nobel <bear.nobel@gmail.com>
2016-01-28 12:05:38 -0500

    added comments

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -13,52 +13,55 @@ public class Assignment1
 {
     public static void main( String[] args )
     {
-        Scanner in = new Scanner( System.in );
+        Scanner in = new Scanner( System.in ); //............... for user input
 
-        System.out.print( "Enter an expression to test: ");
-        testThisString( in.nextLine() );
+        System.out.print( "Enter an expression to test: "); //.. prompt user for input
+        demorganThisString( in.nextLine() ); //................. capture full line input test the method
     }
 
-    public static void testThisString( String input )
+    // Method to spit out the De Morgan equivalent
+    // takes a String argument
+    // prints out the De Morgan equivalent
+    public static void demorganThisString( String input )
     {
-        ArrayList<Object> queue = new ArrayList<>();
-        StringTokenizer string = new StringTokenizer( input );
-        String toCheck;
+        ArrayList<Object> queue = new ArrayList<>(); //.......................... the queue
+        StringTokenizer string = new StringTokenizer( input ); //................ tokenizer to split up the string
+        String toCheck; //....................................................... variable to hold the String stripped by tokenizer
 
-        while( string.hasMoreElements() )
+        while( string.hasMoreElements() ) //..................................... loop through each element in String
         {
-            toCheck = ( (String)string.nextElement() ).toUpperCase();
+            toCheck = ( (String)string.nextElement() ).toUpperCase(); //......... capture the first element
 
-            if( toCheck.equals("NOT") )
+            if( toCheck.equals("NOT") ) //....................................... if it equals NOT
             {
-                while( string.hasMoreElements() )
+                while( string.hasMoreElements() ) //............................. loop through the remaining elements
                 {
-                    toCheck = ( (String)string.nextElement() ).toUpperCase();
+                    toCheck = ( (String)string.nextElement() ).toUpperCase(); //. capture the first element
 
-                    switch( toCheck )
+                    switch( toCheck ) //......................................... drop captured element into switch
                     {
-                        case "AND":
+                        case "AND": //........................................... when it equals AND add OR to queue
                             queue.add( "OR " );
                             break;
 
-                        case "OR":
+                        case "OR": //............................................ when it equals OR add AND to queue
                             queue.add( "AND " );
                             break;
 
                         case "(":
-                        case ")":
-                            queue.add( String.format("%s ", toCheck) );
+                        case ")": //............................................. if its either paren
+                            queue.add( String.format("%s ", toCheck) ); //....... add to queue
                             break;
 
-                        default:
-                            queue.add( String.format("(NOT %s) ", toCheck) );
+                        default: //.............................................. if its anything else
+                            queue.add( String.format("(NOT %s) ", toCheck) ); //. add NOT to it and toss onto queue
                             break;
                     }
                 }
 
-                for( Object item: queue ) System.out.print( item );
+                for( Object item: queue ) System.out.print( item ); //........... empty queue and print elements
             }
-            else System.out.print( String.format("%s ", toCheck) );
+            else System.out.print( String.format("%s ", toCheck) ); //........... print out the element
         }
     }
 }
